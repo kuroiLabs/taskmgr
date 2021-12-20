0696fbb92244a5c8ab3f83c8b54613573bc72b7b
kuro <kuro@kuroi.io>
2021-12-20 12:50:03 -0800

    Create index.ts

---
+++ src/threads/index.ts
@@ -0,0 +1,6 @@
+export * from "./batch-job";
+export * from "./job.interface";
+export * from "./worker-dependency.interface"
+export * from "./thread-manager"
+export * from "./worker-thread"
+export * from "./worker-thread.interface"
